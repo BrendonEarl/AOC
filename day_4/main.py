f7ec87e09d349e9ffbf3ed2d05ad64fe245f67bc
Brendon Earl <63430919+Brennypoo@users.noreply.github.com>
2022-12-06 22:28:55 +0000

    up to day 6

---
+++ day_4/main.py
@@ -0,0 +1,22 @@
+def main(filename):
+    arr = [line.strip().split(",") for line in open(filename).readlines()]
+    is_contained = lambda one, two: \
+        (one[0] >= two[0] and one[1] <= two[1]) or \
+        (one[0] <= two[0] and one[1] >= two[1])
+    is_overlapping = lambda one, two: \
+        (one[0] in range(two[0], two[1] + 1) or
+         one[1] in range(two[0], two[1] + 1)) or \
+        (two[0] in range(one[0], one[1] + 1) or
+         two[1] in range(one[0], one[1] + 1))
+
+    totalcontained, totaloverlapping= 0, 0
+    for line in arr:
+        first, second = [int(num) for num in line[0].split("-")], [int(num) for num in line[1].split("-")]
+        totaloverlapping += 1 if is_overlapping(first,second) else 0
+        totalcontained += 1 if is_contained(first,second) else 0
+
+    print(f'{totalcontained} \n'
+          f'{totaloverlapping}')
+
+
+main('resources/input_4.txt')
---
+++ day_6/main.py
@@ -0,0 +1,23 @@
+import queue
+
+
+def main(filename, part2=True) -> int:
+    trail = open(filename).read().strip()
+    crawler = queue.Queue(maxsize:=14 if part2 else 4)
+    grab, drop, crumbs = lambda crumb: crawler.put(crumb), lambda: crawler.get(), lambda: crawler.queue
+    [grab(crumb) for crumb in trail[:maxsize]]
+    
+    for step, crumb in enumerate(trail):
+        unique = True
+        for _ in range(maxsize):
+            if (inspect := drop()) in crumbs():
+                unique = False
+            grab(inspect)
+        if unique:
+            return step
+        drop()
+        grab(crumb)
+
+filepath = "resources/input_6.txt"
+print(f'part1: {main(filepath, False)} \n'
+      f'part2: {main(filepath)}')
