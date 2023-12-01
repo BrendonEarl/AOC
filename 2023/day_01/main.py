2f0d84f82834d85e6f92972e847bfef23da169a3
BrendonEarl <63430919+BrendonEarl@users.noreply.github.com>
2023-12-01 15:57:06 +0000

    day 01

--- 2023/day_01/main.py
+++ 2023/day_01/main.py
@@ -1,7 +1,54 @@
+map = {
+    'one' : 1,
+    'two' : 2,
+    'three' : 3,
+    'four' : 4,
+    'five' : 5,
+    'six' : 6,
+    'seven' : 7,
+    'eight' : 8,
+    'nine' : 9,
+}
 
+def part1(filename):
+    test = [line.strip() for line in open(filename).readlines()]
+    sum = 0
+    
+    for y in test:
+        found = False
+        first = 0
+        last = 0
+        for z in y:
+            if(z.isnumeric()):
+                if not found:
+                    first = int(z)
+                    found = True
+                last = int(z)
+        sum += (first * 10 ) + last
+    print(sum)
 
-def main(filename, part1=True):
-    test = [print(line, end=' ') for line in open(filename).readlines()]
+def main(filename):
+    test = [line.strip() for line in open(filename).readlines()]
+    sum = 0
 
-real = '2023/resources/test.txt'
-main(real)
+    for line in test:
+        first = 0
+        last = 0
+        for index, char in enumerate(line):
+            if(char.isnumeric()):
+                if not first:
+                    first = int(char)
+                last = int(char)
+            for i in range(3,6):
+                try:
+                    if not first:
+                        first = map[line[index:index+i]]
+                    last = map[line[index:index+i]]
+                except:
+                    KeyError
+        sum += (first * 10 ) + last
+    print(sum)
+                    
+f = '2023/resources/input_01.txt'
+part1(f)
+main(f)
